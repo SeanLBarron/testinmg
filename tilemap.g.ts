2fc6ccc7b20f395526b92f77c51baab6641a73e8
SeanLBarron <167994373+SeanLBarron@users.noreply.github.com>
2024-04-25 18:11:48 -0400

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,74 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000200004050505050505050505050505050506030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07030b0b0b0b0b0b0b0b0b0b0b0b0b0b07090b0b0b0b0b0b0b0b0b0b0b0b0b0b0a01020202020202020202020202020208`, img`
+2222222222222222
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2..............2
+2222222222222222
+`, [myTiles.transparency16,sprites.builtin.forestTiles16,sprites.builtin.forestTiles2,sprites.builtin.forestTiles9,sprites.builtin.forestTiles5,sprites.builtin.forestTiles6,sprites.builtin.forestTiles7,sprites.builtin.forestTiles11,sprites.builtin.forestTiles20,sprites.builtin.forestTiles13,sprites.builtin.forestTiles15,sprites.builtin.forestTiles14], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
